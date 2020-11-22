14008191a5db7c07e28affad4f3d745167e346f2
Tig0r <christopher.putz@sheep-records.de>
2020-11-22 16:33:15 +0100

    added first part of pixel post processing shader

---
+++ Assets/PostProcessing/BlitRenderPass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class BlitRenderPass : ScriptableRenderPass
+{
+    public RenderTargetIdentifier source;
+
+    private Material material;
+    private RenderTargetHandle tempRenderTargetHandler;
+
+    public BlitRenderPass(Material material)
+    {
+        this.material = material;
+
+        tempRenderTargetHandler.Init("randomName");
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        CommandBuffer commandBuffer = CommandBufferPool.Get();
+        commandBuffer.GetTemporaryRT(tempRenderTargetHandler.id, renderingData.cameraData.cameraTargetDescriptor);
+
+        Blit(commandBuffer, source, tempRenderTargetHandler.Identifier(), material);
+        Blit(commandBuffer, tempRenderTargetHandler.Identifier(), source);
+
+        context.ExecuteCommandBuffer(commandBuffer);
+        CommandBufferPool.Release(commandBuffer);
+    }
+}
---
+++ Assets/PostProcessing/BlitRenderPassFeature.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class BlitRenderPassFeature : ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class Settings
+    {
+        public Material material = null;
+    }
+
+    public Settings settings = new Settings();
+    private BlitRenderPass renderPass;
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderPass.source = renderer.cameraColorTarget;
+        renderer.EnqueuePass(renderPass);
+    }
+
+    public override void Create()
+    {
+        renderPass = new BlitRenderPass(settings.material);
+
+        renderPass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    }
+}
