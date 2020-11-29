51fb548b496d117d7b30147a37cf35273af8ed9e
Tig0r <christopher.putz@sheep-records.de>
2020-11-29 18:15:28 +0100

    added a main menu

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("CharacterCreationMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
