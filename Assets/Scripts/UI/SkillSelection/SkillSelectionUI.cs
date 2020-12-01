0eacfd1532c67a50271a548d601ba946accebca3
Tig0r <christopher.putz@sheep-records.de>
2020-12-01 11:36:53 +0100

    Added switching between scenes

---
+++ Assets/Scripts/UI/SkillSelection/SkillSelectionUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkillSelectionUI : MonoBehaviour
+{
+    public IntAttributeObject test;
+
+    private void Start()
+    {
+        Debug.Log(test.Value);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("CharacterCreationMenu");
+    }
+
+    public void Next()
+    {
+        //SceneManager.LoadScene("");
+    }
+}
