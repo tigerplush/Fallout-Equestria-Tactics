8e0ca80bc3c4a97750eb78d52c9160c2af103f6d
Tig0r <christopher.putz@sheep-records.de>
2020-12-01 11:41:46 +0100

    Display default name on load

--- Assets/Scripts/UI/CharacterCreation/NameInputFieldUI.cs
+++ Assets/Scripts/UI/CharacterCreation/NameInputFieldUI.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NameInputFieldUI : MonoBehaviour
 {
+    public TMP_InputField inputField;
     public CharacterObject character;
+
+    public void OnEnable()
+    {
+        inputField.text = character.CharacterName;
+    }
 
     public void OnValueChanged(string value)
     {
