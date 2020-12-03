9d8109ec058b9e827fde59c131ba2c3e70156b3a
Tig0r <christopher.putz@sheep-records.de>
2020-12-03 13:01:15 +0100

    Added multiply and division node to formula editor

---
+++ Assets/Scripts/Math/MultiplyNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class MultiplyNode : MathNode
+{
+    [Input] public float a;
+    [Input] public float b;
+    [Output] public float product;
+    // Use this for initialization
+    protected override void Init()
+    {
+        base.Init();
+    }
+
+    // Return the correct value of an output port when requested
+    public override object GetValue(NodePort port)
+    {
+        float a = GetInputValue<float>("a", this.a);
+        float b = GetInputValue<float>("b", this.b);
+        return a * b; // Replace this
+    }
+}
