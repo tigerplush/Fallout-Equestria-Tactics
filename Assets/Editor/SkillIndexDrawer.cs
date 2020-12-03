27755048899082b94d064f56c724f46d2038c841
Tig0r <christopher.putz@sheep-records.de>
2020-12-03 18:03:53 +0100

    fixed bug where elements wouldn't be created

--- Assets/Editor/SkillIndexDrawer.cs
+++ Assets/Editor/SkillIndexDrawer.cs
@@ -9,11 +9,11 @@ public class SkillIndexDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         SerializedObject skillIndex = new SerializedObject(property.objectReferenceValue);
-        SerializedProperty skillElements = skillIndex.FindProperty("indexElements");
+        SerializedProperty elementsToCreate = skillIndex.FindProperty("elementsToCreate");
         SerializedProperty skillValues = skillIndex.FindProperty("indexValues");
 
         EditorGUI.BeginProperty(position, label, property);
-        EditorGUILayout.PropertyField(skillElements);
+        EditorGUILayout.PropertyField(elementsToCreate);
         if (GUILayout.Button("Create Skills"))
         {
             SkillIndex index = skillIndex.targetObject as SkillIndex;
