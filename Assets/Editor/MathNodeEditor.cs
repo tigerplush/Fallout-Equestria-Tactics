4a734cfee829c8d764b78f0c9d5d50bd48ad63c8
Tig0r <christopher.putz@sheep-records.de>
2020-12-03 20:08:41 +0100

    Added red shade for output nodes

---
+++ Assets/Editor/MathNodeEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using XNodeEditor;
+
+[CustomNodeEditor(typeof(MathNode))]
+public class MathNodeEditor : NodeEditor
+{
+    public override Color GetTint()
+    {
+        MathNode node = target as MathNode;
+        Formula formula = node.graph as Formula;
+        if(node == formula.output)
+        {
+            return new Color(178f / 255f, 24f / 255f, 34f / 255f);
+        }
+        return base.GetTint();
+    }
+}
